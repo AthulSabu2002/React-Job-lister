b6b2a51eea39782909469b3d07c042bea5df5ecd
athul <athulsabu75@gmail.com>
2024-05-05 19:05:13 +0530

    added pages and routing feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListing from './components/JobListing';
+import MainLayout from './layouts/MainLayout'
+import HomePage from './Pages/HomePage'
+import JobsPage from './Pages/JobsPage';
+import NotFoundPage from './Pages/NotFoundPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={ <HomePage /> }/> 
+      <Route path='/jobs' element={ <JobsPage /> }/> 
+      <Route path='*' element={ <NotFoundPage /> }/>
+    </Route>
+  
+));
+
+
 
 const App = () => {
-  return (
-    <>
-    <Navbar />
-
-    <Hero />
-
-    <HomeCards />
-    
-    <JobListing />
-    
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
-    </>
-  )
-}
+  return <RouterProvider router={router} />
+};
 
 export default App
