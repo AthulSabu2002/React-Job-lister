e24a6d153fd87a479c4cd4c4429ac7e703f6dbd9
athul <athulsabu75@gmail.com>
2024-05-08 18:35:11 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ const router = createBrowserRouter(
   createRoutesFromElements(
 
     <Route path='/' element={<MainLayout />}>
-      <Route index element={ <HomePage /> }/> 
-      <Route path='/jobs' element={ <JobsPage /> }/> 
-      <Route path='*' element={ <NotFoundPage /> }/>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Route>
-  
-));
+
+  ));
 
 
 
